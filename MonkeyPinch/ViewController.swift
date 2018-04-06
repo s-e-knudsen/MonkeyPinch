fc34623416dd2c3d26bc969143bdc88db4e64563
SorenEKnudsen <soren.egede.knudsen@gmail.com>
2018-04-06 17:19:20 +0200

    test pan gesture via gui

--- MonkeyPinch/ViewController.swift
+++ MonkeyPinch/ViewController.swift
@@ -37,6 +37,16 @@ class ViewController: UIViewController {
     
   }
   
+@IBAction func handlePan(recognizer:UIPanGestureRecognizer) {
+    let translation = recognizer.translation(in: self.view)
+    if let view = recognizer.view {
+            view.center = CGPoint(x:view.center.x + translation.x,
+                                  y:view.center.y + translation.y)
+        }
+        recognizer.setTranslation(CGPoint.zero, in: self.view)
+    }
+    
+    
   @IBAction func handlePinch(recognizer : UIPinchGestureRecognizer) {
     
   }
